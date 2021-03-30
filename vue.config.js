8225a43675aa505f671fe7c08f256bf2f6864889
jamesho <jamesho@flytech.com.tw>
2021-03-30 17:20:39 +0800

    test-2

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/'
-  : './'
-  // publicPath: './',
+  // publicPath: process.env.NODE_ENV === 'production'
+  // ? '/'
+  // : './'
+  publicPath: './',
   // outputDir: path.resolve(__dirname, 'dist')
 }
