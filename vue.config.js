c42b48b27279b787682698c12decf1dea1848cc6
jamesho <jamesho@flytech.com.tw>
2021-03-30 17:15:37 +0800

    test-1

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/viper-file'
+  ? '/'
   : './'
   // publicPath: './',
   // outputDir: path.resolve(__dirname, 'dist')
