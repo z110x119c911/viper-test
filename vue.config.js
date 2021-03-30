505cd8763d25d1fdea84091e5d94ff1a5e4fb18b
jamesho <jamesho@flytech.com.tw>
2021-03-30 18:40:12 +0800

    test-3

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production'
-  // ? '/'
-  // : './'
-  publicPath: './dist/',
+  publicPath: process.env.NODE_ENV === 'production'
+  ? 'viper-test'
+  : './dist/',
+  // publicPath: './dist/',
   // outputDir: '',
   // assetsDir:'../',
   indexPath:'../index.html/',
